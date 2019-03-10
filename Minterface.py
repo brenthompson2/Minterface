201ccbcde81f1be63415c706dde517b183ce4e0a
brenthompson2 <brenthompson2@gmail.com>
2019-03-10 11:17:13 -0400

    Removed TODO handled in last commit

--- Minterface.py
+++ Minterface.py
@@ -20,7 +20,6 @@ mint_analyzer = MintAnalyzer(None)
 account_balances_over_time = mint_analyzer.get_account_balances_over_time()
 account_balances_over_time_data = []
 for account_key, account in account_balances_over_time.items():
-    # TODO: Ensure debt accounts have negative balances
     account_balances_over_time_data.append({'x': account['dates'], 'y': account['balances'], 'type':'line', 'name':account_key})
 
 # Draw Dash app layout
